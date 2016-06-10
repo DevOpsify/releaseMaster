380b96140cadeca0873c08688a55c36890ce81f6
Max Cai <zhiyuan.cai@gmail.com>
2016-06-09 22:37:42 -0400

    add mysql ds

---
+++ server/create-lb-tables.js
@@ -0,0 +1,9 @@
+
+var server = require('./server');
+var ds = server.dataSources.db;
+var lbTables = ['User', 'AccessToken', 'ACL', 'RoleMapping', 'Role'];
+ds.automigrate(lbTables, function(er) {
+  if (er) throw er;
+  console.log('Loopback tables [' + lbTables + '] created in ', ds.adapter.name);
+  ds.disconnect();
+});
