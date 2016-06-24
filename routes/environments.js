0ed92cf8fe7bc0d1426111daeebdff8501d3f491
siweiwang <wangsiwei2003@hotmail.com>
2016-06-24 01:11:42 -0400

    code health -- Use Async js for cleaner code base
    user aysnc js to handle callbacks
    
    update env endpoint

--- routes/environments.js
+++ routes/environments.js
@@ -4,54 +4,69 @@ var HTTPStatus = require('http-status');
 
 var mongoose = require('mongoose');
 var dbschema = require('../models/dbschema.js');
+
+var async = require('async')
+
 var Environment = dbschema.Environment;
 
 /* Gets all environments. */
 router.get('/', function(req, res, next) {
-    Environment.find(function (err, environments) {
+    async.waterfall([
+        function (callback){
+            Environment.find(callback)
+        }
+    ],function (err, environments){
         if (err) return next(err);
         res.json(environments);
-    });
+    })
 });
 
 /* Create a environment */
 // Check duplicate on name
 router.post('/', function(req, res, next) {
-    console.log(req.body);
-    Environment.findOne({ 'name': req.body.name }, 'name ', function (err, environment) {
+    async.waterfall([
+        function (callback){
+            if (req.body.name){
+                Environment.findOne({ 'name': req.body.name }, 'name', callback)
+            }else{
+                res.status(HTTPStatus.BAD_REQUEST).json({"reason": "Name is not set in the payload"})
+            }
+        }
+    ],function (err, environment){
         if (err) return handleError(err);
-        if (environment) {
-            console.log("duplicate");
-            res.status(HTTPStatus.NOT_MODIFIED).json(environment);
-        }
-        else
-        {
-            var newEnvironment = new Environment(req.body);
-            newEnvironment.save(function(err){
-                if (err) return next(err);
-                res.json(newEnvironment);
-            });
-        }
+        if (environment) {res.status(HTTPStatus.NOT_MODIFIED).end(); }
+        var newEnvironment = new Environment(req.body);
+        newEnvironment.save(function(err){
+            if (err) return next(err);
+            res.json(newEnvironment);
+        });
     });
 });
 
-/* PUT /environments/:id */
+/* Updates the environment by id*/
 // Todo: check duplicate before update the environment name?
 router.put('/id/:id', function(req, res, next) {
-    Environment.findByIdAndUpdate(req.params.id, req.body, function (err, post) {
+    async.waterfall([
+        function (callback) {
+            var newEnvironment = new Environment(req.body);
+            Environment.findByIdAndUpdate(req.params.id, newEnvironment, callback)
+        }
+    ],function (err, environment){
         if (err) return next(err);
-        res.json(post);
+        res.json(environment);
     });
 });
 
-/* DELETE /environments/:id */
+/* Delete the environments by id */
 // Todo: delete all deployments for the environment
 router.delete('/id/:id', function(req, res, next) {
-    Build.find({application: req.params.id}).remove().exec(function(err){
-        Environment.findByIdAndRemove(req.params.id, function (err, post) {
-            if (err) return next(err);
-            res.json(post);
-        });
+    async.waterfall([
+        function (callback){
+            Environment.findByIdAndRemove(req.params.id, callback);
+        }
+    ], function (err, environment){
+        if (err) return next(err);
+        res.json(environment)
     });
 });
 
