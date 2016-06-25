ae1bbd114b01ea39f9e6c8ce4180582cb618f7d2
siweiwang <wangsiwei2003@hotmail.com>
2016-06-25 19:28:04 -0400

    user aysnc js to handle callbacks

--- routes/applications.js
+++ routes/applications.js
@@ -4,87 +4,65 @@ var HTTPStatus = require('http-status');
 
 var mongoose = require('mongoose');
 var dbschema = require('../models/dbschema.js');
+
+var async = require('async')
+
 var Application = dbschema.Application;
 var Build = dbschema.Build;
 
 /* Gets all applications. */
 router.get('/', function(req, res, next) {
-    Application.find(function (err, applications) {
-        if (err) return next(err);
+    async.waterfall([
+        function (callbacke){
+            Application.find(callbacke);
+        }
+    ],function (err,applications){
+        if (err) next (err);
         res.json(applications);
     });
 });
 
 /* Creates a new application */
 router.post('/', function(req, res, next) {
-    Application.findOne({ 'name': req.body.name }, 'name ', function (err, application) {
-        if (err){
-            return handleError(err);
+    async.waterfall([
+        function (callback){
+            Application.findOne({ 'name': req.body.name }, 'name', callback);
         }
-        if (application){
-            console.log("duplicate");
-            var res_json = {
-                "reason": "Appliction name already exists"
-            }
-            res.status(HTTPStatus.NOT_MODIFIED).json(res_json);
-        }
-        else {
-            var newApplication = new Application(req.body);
-            newApplication.save(function(err){
-                if (err) return next(err);
-            });
-            res.json(newApplication);
-        }
+    ],function (err, application){
+        if (err) return handleError(err);
+        if (application) res.status(HTTPStatus.NOT_MODIFIED).end();
+        var newApplication = new Application(req.body);
+        newApplication.save(function(err){ if (err) return next(err); });
+        res.json(newApplication);
     });
-
 });
 
-
 /* Gets application by name*/
-
-// Get all builds for this application
-// Todo: limit the return size ?
 router.get('/name/:name', function(req, res, next) {
-    Application.findOne( {'name': req.params.name }, function(err,application){
-        if (err) return next(err);
-        if (application==null) {
-            res.status(HTTPStatus.NOT_FOUND).end();
-            return;
+    async.waterfall([
+        function (callback){
+            Application.findOne( {'name': req.params.name },callback);
         }
-
-        Build.find ({application: application._id})
-        .sort("created_at")
-        .exec(function (err, post) {
-            if (err) return next(err);
-            fulljson = { application : application.name,
-                description : application.description,
-                applicationid : application._id,
-                builds : post };
-            res.json(fulljson);
-        });
+    ], function (err, application){
+        if (err) return next(err);
+        if (application){
+            res.json(application);
+        } else {
+            res.status(HTTPStatus.NOT_FOUND).json({"reason":"Applicaion name not found"});
+        }
     });
 });
 
 /* Gets the application by id*/
 router.get('/id/:id', function(req, res, next){
-     Application.findById(req.params.id, function(err, application){
-        if (err) return next(err);
-        
-        if (application==null) {
-            res.status(HTTPStatus.NOT_FOUND).end();
-            return;
+     async.waterfall([
+        function (callback){
+            Application.findById(req.params.id, callback);
         }
- 
-        Build.find ({application: application._id})
-        .sort("created_at")
-        .exec(function (err, post) {
-            if (err) return next(err);
-            fulljson = { application : application.name,
-                description : application.description,
-                applicationid : application._id,
-                builds : post };
-            res.json(fulljson);
-        });
+     ],function (err, application){
+        if (err) return next(err);
+        if (application) { res.json(application); }
+        res.status(HTTPStatus.NOT_FOUND).end();
      });
 });
 
@@ -92,22 +70,28 @@ router.get('/id/:id', function(req, res, next){
 /* Updates the application by id */
 // Todo: check duplicate before update the application name?
 router.put('/id/:id', function(req, res, next) {
-    Application.findByIdAndUpdate(req.params.id, req.body, function (err, post) {
-        if (err) return next(err);
-        res.json(post);
+    async.waterfall([
+        function (callback) {
+            var newApplication = new Application(req.body)
+             Application.findByIdAndUpdate(req.params.id, newApplication, callback);
+        }
+    ], function(err, application){
+         if (err) return next(err);
+        res.json(application);
     });
 });
 
-/* Deletes the application by name */
+/* Deletes the application by id */
 // Todo: delete all buils and deployments for the application
 router.delete('/id/:id', function(req, res, next) {
-    Build.find({application: req.params.id}).remove().exec(function(err){
-        Application.findByIdAndRemove(req.params.id, req.body, function (err, post) {
-            if (err) return next(err);
-            res.json(post);
-        });
+    async.waterfall([
+        function (callback) {
+            Application.findByIdAndRemove(req.params.id, callback) ;
+        }
+    ], function (err, application){
+        if (err) return next(err);
+        res.json(application);
     });
-
 });
 
 module.exports = router;
