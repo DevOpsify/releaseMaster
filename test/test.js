21dc6221cb4edc23f39a404c55f28f7ce69189a3
Qiang Wu <tomoodesign@gmail.com>
2016-10-09 23:40:47 -0400

    add html content-type check

--- test/test.js
+++ test/test.js
@@ -1,15 +1,20 @@
 var request = require("supertest");
 var app = require("../app");
 
-describe('requests to root', function() {
-  it('responds with 200', function(done) {
-       request(app)
-           .get('/')
-           .expect(200)
-           .end(function(error){
-               if (error) throw error;
-               done();
-           });
-  });
+describe('Requests to root path', function () {
+    it('responds with 200', function (done) {
+        request(app)
+            .get('/')
+            .expect(200, done());
+    });
+
+    it('Returns html', function (done) {
+        request(app)
+            .get('/')
+            .expect('Content-Type', /html/, done);
+    });
 });
+
+
+
 
