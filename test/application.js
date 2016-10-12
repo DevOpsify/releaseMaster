1ce0650917f529b414f0cc501c0afddd2a8ebb3c
Qiang Wu <tomoodesign@gmail.com>
2016-10-11 21:18:58 -0400

    seperate test files

--- test/application.js
+++ test/application.js
@@ -1,3 +1,36 @@
 /**
  * Created by tom on 2016-10-11.
  */
+
+var request = require("supertest");
+var app = require("../app");
+var HTTPStatus = require('http-status');
+var dbschema = require('../models/dbschema.js');
+
+var Application = dbschema.Application;
+
+describe('Requests to Applications', function () {
+
+    before(function () {
+        Application.remove({}, function(error){
+            if (error) throw error;
+        });
+    });
+
+    it('should get 200 and json', function (done) {
+        request(app)
+            .get('/applications')
+            .expect('Content-Type', /application\/json/)
+            .expect(HTTPStatus.OK, done)
+    });
+
+    it('should get 201 and json', function (done) {
+        request(app)
+            .post('/applications')
+            .send({name: "ApplicationTest", description: "This is an application description"})
+            .expect('Content-Type', /application\/json/)
+            .expect(HTTPStatus.CREATED, done);
+    });
+
+});
+
--- test/base.js
+++ test/base.js
@@ -1,9 +1,6 @@
 var request = require("supertest");
 var app = require("../app");
 var HTTPStatus = require('http-status');
-var dbschema = require('../models/dbschema.js');
-
-var Application = dbschema.Application;
 
 describe('Requests to root path', function () {
     it('should response with 200 and html content-type', function (done) {
@@ -12,30 +9,5 @@ describe('Requests to root path', function () {
             .expect(HTTPStatus.OK)
             .expect('Content-Type', /html/, done);
     });
-});
-
-describe('Requests to Applications', function () {
-
-    before(function () {
-        Application.remove({}, function(error){
-            if (error) throw error;
-        });
-    });
-
-    it('should get 200 and json', function (done) {
-        request(app)
-            .get('/applications')
-            .expect('Content-Type', /application\/json/)
-            .expect(HTTPStatus.OK, done)
-    });
-
-    it('should get 201 and json', function (done) {
-        request(app)
-            .post('/applications')
-            .send({name: "ApplicationTest", description: "This is an application description"})
-            .expect('Content-Type', /application\/json/)
-            .expect(HTTPStatus.CREATED, done);
-    });
-
 });
 
