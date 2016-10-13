d4a214f7084f4e7baf7bd5c940e85d76c8f3e56a
Shi Chen <shixchen@deloitte.ca>
2016-10-13 12:24:37 -0400

    add mutiple select of profiles

--- public/javascripts/controllers/profile-controller.js
+++ public/javascripts/controllers/profile-controller.js
@@ -2,6 +2,12 @@
 
 angular.module('release-master').controller('ProfileController', function($scope, $routeParams, $http, Profiles) {
   var encoded = encodeURIComponent($routeParams.application);
+  $scope.checked = 0;
+  $scope.limit = 2;
+  $scope.checkChanged = function(profile){
+        if(profile.selected) $scope.checked++;
+        else $scope.checked--;
+    }
 
   $http.
     get('/profiles/?application=' + encoded).
