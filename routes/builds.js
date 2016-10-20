6f53d30a469999d92da66d9525dafcdb33576aeb
siweiwang <wangsiwei2003@hotmail.com>
2016-10-20 01:00:45 -0400

    add build seeds file

--- routes/builds.js
+++ routes/builds.js
@@ -86,8 +86,6 @@ router.route('/')
           res.status(HTTPStatus.NOT_FOUND).json(res_json);
           return
         }
-
-
         var query = Build.find({});
         query.where("application", application._id);
         if (req.query.branch) {
